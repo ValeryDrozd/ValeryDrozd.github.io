737f9757fd2083e111139d84df476829c1c21524
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-25 04:25:35 +0200

    Add files via upload

--- js/getpage.js
+++ js/getpage.js
@@ -1,6 +1,6 @@
-/*function getMainPage(){
+
     
-    return   '<div id = "promos">'+
+let routes = {'main': '<div id = "promos">'+
     '<div id="carousel">'+
     '<img src="./images/promo1.jpg">'+
     '<img src="./images/promo2.jpg">'+
@@ -10,29 +10,13 @@
         '<button id="nextbutton" onclick="slideright()">Next</button>'+
     '</div>'+
     '</div>'+
-
-'<hr>'+
-'<span><h1 style="margin-left:10px;margin-top: 0px;">The most popular orders</h1></span>'+  
-'<div class = "goods" id="goodsField">'+
-'    <img src="./images/loading.svg" style="width: 100%;height: 250px;">'+
-'</div>';
-
-}
-
-function getAllPage(){
-
-}
-function getSpecPage(){
-    
-}
-function getItemPage(){
-
-}
-
-function getCartPage(){
-
-}
-
-export {getMainPage};*/
-const a = 1;
-export {a}
+    '<hr>'+
+    '<span><h1 style="margin-left:10px;margin-top: 0px;">The most popular orders</h1></span>'+  
+    '<div class = "goods" id="goodsField">'+
+    '    <img src="./images/loading.svg" style="width: 100%;height: 250px;">'+
+    '</div>',
+    'cart':'',
+    'sushi':'',
+    'pizza':'',
+    'drinks':''
+};
