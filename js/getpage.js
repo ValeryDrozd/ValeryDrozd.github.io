0f86f7ee7c6d396e2f4214f8e1da04d2e8801241
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-25 22:51:09 +0200

    forth method to fix that error

--- js/getpage.js
+++ js/getpage.js
@@ -1,28 +1,28 @@
 
     
 export let routes = {
-    'header':' <button id="mainBtn" onclick="router("")">'+
+    'header':' <button id="mainBtn" onclick=\'router(" ")\'>'+
    ' Main'+
 '</button>'+
-'<button id="allBtn" onclick="jump("#all")">'+
+'<button id="allBtn" onclick=\'jump("#all")\'>'+
  '   Catalogue'+
 '</button>'+
-'<button id="pizzaBtn" onclick="jump("#pizza")">'+
+'<button id="pizzaBtn" onclick=\'jump("#pizza")\'>'+
 'Pizza'+
 '</button>'+
-'<button id="sushiBtn" onclick="jump("#sushi")">'+
+'<button id="sushiBtn" onclick=\'jump("#sushi")\'>'+
 'Sushi'+
 '</button>'+
-'<button id="drinkBtn" onclick="jump("#drinks")">'+
+'<button id="drinkBtn" onclick=\'jump("#drinks")\'>'+
 'Drinks'+
 '</button>'+
-'<button id="cartBtn" style="float:right;" onclick="router("#cart")">'+
+'<button id="cartBtn" style="float:right;" onclick=\'router(#cart)\'>'+
 '    In the cart: <span id="amount">0</span>'+
 '</button>',
 
     'main': '<div id = "promos">'+
     '<div id="carousel">'+
-    '    <img src="./images/loading.svg" style="width: 100%;height: 100%;">'+
+    '    <img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 100%;">'+
     '</div>'+
     '<div id="buttons">'+
         '<button id="prevbutton" onclick="slideleft()">Previous</button>'+
@@ -32,16 +32,16 @@ export let routes = {
     '<hr>'+
     '<span><h1 style="margin-left:10px;margin-top: 0px;">The most popular orders</h1></span>'+  
     '<div class = "goods" id="goodsField">'+
-    '    <img src="./images/loading.svg" style="width: 100%;height: 250px;">'+
+    '    <img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;">'+
     '</div>',
     'cart':'',
-    'sushi':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
-    'pizza':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
-    'drinks':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
+    'sushi':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>',
+    'pizza':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>',
+    'drinks':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>',
     'all':'<span><h1 style="margin-left:10px;margin-top: 0px;">Pizzas</h1></span>'+
-          '<div class = "goods" id="PizzaField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'+
+          '<div class = "goods" id="PizzaField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>'+
           '<span><h1 style="margin-left:10px;margin-top: 0px;">Sushies</h1></span>'+
-          '<div class = "goods" id="SushiField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'+
+          '<div class = "goods" id="SushiField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>'+
           '<span><h1 style="margin-left:10px;margin-top: 0px;">Drinks</h1></span>'+
-          '<div class = "goods" id="DrinksField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'
+          '<div class = "goods" id="DrinksField"><img src="./images/loading.svg" alt="loadimage" style="width: 100%;height: 250px;"></div>'
 };
