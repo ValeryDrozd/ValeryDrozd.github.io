4ed28ba4f3eb82af857f46fc5d10e3584db8a685
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-25 22:36:55 +0200

    Trying to fix the bug

--- js/getpage.js
+++ js/getpage.js
@@ -1,6 +1,26 @@
 
     
-export let routes = {'main': '<div id = "promos">'+
+export let routes = {
+    'header':' <button id="mainBtn" onclick="router("")">'+
+   ' Main'+
+'</button>'+
+'<button id="allBtn" onclick="jump("#all")">'+
+ '   Catalogue'+
+'</button>'+
+'<button id="pizzaBtn" onclick="jump("#pizza")">'+
+'Pizza'+
+'</button>'+
+'<button id="sushiBtn" onclick="jump("#sushi")">'+
+'Sushi'+
+'</button>'+
+'<button id="drinkBtn" onclick="jump("#drinks")">'+
+'Drinks'+
+'</button>'+
+'<button id="cartBtn" style="float:right;" onclick="router("#cart")">'+
+'    In the cart: <span id="amount">0</span>'+
+'</button>',
+
+    'main': '<div id = "promos">'+
     '<div id="carousel">'+
     '    <img src="./images/loading.svg" style="width: 100%;height: 100%;">'+
     '</div>'+
@@ -15,7 +35,13 @@ export let routes = {'main': '<div id = "promos">'+
     '    <img src="./images/loading.svg" style="width: 100%;height: 250px;">'+
     '</div>',
     'cart':'',
-    'sushi':'',
-    'pizza':'',
-    'drinks':''
+    'sushi':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
+    'pizza':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
+    'drinks':'<div class = "goods" id="goodsField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>',
+    'all':'<span><h1 style="margin-left:10px;margin-top: 0px;">Pizzas</h1></span>'+
+          '<div class = "goods" id="PizzaField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'+
+          '<span><h1 style="margin-left:10px;margin-top: 0px;">Sushies</h1></span>'+
+          '<div class = "goods" id="SushiField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'+
+          '<span><h1 style="margin-left:10px;margin-top: 0px;">Drinks</h1></span>'+
+          '<div class = "goods" id="DrinksField"><img src="./images/loading.svg" style="width: 100%;height: 250px;"></div>'
 };
