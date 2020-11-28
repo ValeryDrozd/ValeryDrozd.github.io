2445c28ee0e5f3369001005d02229d2c093388f4
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-28 18:37:22 +0200

    Fixed id

--- js/makeorder.js
+++ js/makeorder.js
@@ -184,10 +184,10 @@ async function makeorder(){
     userData['dateofexpire'] = document.getElementById('email').value;
     userData['city'] = document.getElementById('city').value;
     if(validate(userData)==true){
-        let id = await fetch("https://my-json-server.typicode.com/ValeryDrozd/Valerydrozd.github.io/orders",{method:'POST'}).then(res => res.json());
+        let id = await fetch("https://my-json-server.typicode.com/ValeryDrozd/Valerydrozd.github.io/categories",{method:'POST'}).then(res => res.json())['id'];
         let orders = getOrder();
         orders['ordercart'][id] = getCart();
-        orders['orderdata'][id] = userData();
+        orders['orderdata'][id] = userData;
         localStorage.setItem('orders',JSON.stringify(orders));
         localStorage.removeItem('cart');
         window.location.hash = '#order/'+id;
