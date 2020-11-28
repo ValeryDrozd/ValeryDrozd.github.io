40465cd5e8b3367087e82bbd8b9b38bd981ed711
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-28 10:29:06 +0200

    Add files via upload

--- js/calcprice.js
+++ js/calcprice.js
@@ -5,11 +5,11 @@ function getSum(){
 
 function increase(productID,productSize,productPrice){
     let basket = localStorage.getItem('cart');
+    basket = JSON.parse(basket);
     if(basket['amount'][String([productID,1*productSize])]==undefined){
         basket['amount'][String([productID,1*productSize])] = 0;
         basket['items'].push([productID,String(productSize)]);
     }
-    basket = JSON.parse(basket);
     basket['amount'][String([productID,1*productSize])]+=1;
     let prevprice = getSum();
     document.getElementById('sum'+productID+productSize).innerText = prevprice+productPrice*1;
