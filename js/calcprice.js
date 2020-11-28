d1cbe56ce172eeccd5dbe054f0ce668130ea705a
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-11-28 10:25:16 +0200

    Fixed testing

--- js/calcprice.js
+++ js/calcprice.js
@@ -5,11 +5,16 @@ function getSum(){
 
 function increase(productID,productSize,productPrice){
     let basket = localStorage.getItem('cart');
+    if(basket['amount'][String([productID,1*productSize])]==undefined){
+        basket['amount'][String([productID,1*productSize])] = 0;
+        basket['items'].push([productID,String(productSize)]);
+    }
     basket = JSON.parse(basket);
     basket['amount'][String([productID,1*productSize])]+=1;
     let prevprice = getSum();
     document.getElementById('sum'+productID+productSize).innerText = prevprice+productPrice*1;
     document.getElementById('allsum').innerText = getSum()+productPrice+'UAH';
+    document.getElementById('amount'+productID+productSize).innerText= basket['amount'][String([productID,1*productSize])];
     localStorage.setItem('cart',JSON.stringify(basket));
     basket['number']++;
     document.getElementById('amount').innerText = basket['number'];
@@ -22,11 +27,16 @@ function decrease(productID,productSize,productPrice){
     if( basket['amount'][String([[productID,1*productSize]])]>0){
         basket['amount'][String([productID,1*productSize])]-=1;
         let prevprice = getSum();
-        document.getElementById('sum'+productID+productSize).innerText = prevprice-productPrice*1;
+        document.getElementById('sum'+productID+productSize).innerText = prevprice-productPrice*1+'UAH';
         document.getElementById('allsum').innerText = getSum()+productPrice+'UAH';
-        localStorage.setItem('cart',JSON.stringify(basket));
         basket['number']--;
+        document.getElementById('amount'+productID+productSize).innerText= basket['amount'][String([productID,1*productSize])];
         document.getElementById('amount').innerText = basket['number'];
+        localStorage.setItem('cart',JSON.stringify(basket));
+        if(basket['amount'][String([productID,1*productSize])]==0){
+            delete basket['amount'][String([[productID,1*productSize]])];
+            basket['items'].splice(basket.indexOf([productID,String(productSize)]));
+        }
         localStorage.setItem('cart',JSON.stringify(basket));
     }
 }
@@ -37,7 +47,7 @@ function remove(productID,productSize,productPrice){
     document.getElementById('sum'+productID+productSize).innerText = getSum() - productPrice*basket['amount'][String([productID,1*productSize])];
     basket['number']-=basket['amount'][String([[productID,1*productSize]])];
     delete basket['amount'][String([[productID,1*productSize]])];
-    basket['items'].splice([productID,String(productSize)]);
+    basket['items'].splice(basket.indexOf([productID,String(productSize)]));
     document.getElementById("elem"+String(productID)+String(productSize)).style.display = 'none';
     if(getSum()==0){
         document.getElementById("orderList").innerHTML = "<h1>Your cart is empty... Buy something!</h1>";
